630b8bec3b2527edc48cf1f16edcfefeeb1738e4
radtina <87929532+radtina@users.noreply.github.com>
2021-07-27 23:11:41 +0800

    Add files via upload

---
+++ explore_page.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+@st.cache
+def load_data():
+    df = pd.read_csv("FYP good data_tensile_today.csv")
+    return df
+
+df = load_data()
+
+def show_explore_page():
+    st.title("Explore compound parameters vs mixer settings")
+
+    st.write(
+        """
+    ### Compound B09255
+    """
+    )
+
+    st.write(
+        """
+    #### Tensile vs Step 7 Energy
+    """
+    )
+
+    data = df.groupby(["Tensile"])["Step7-Energy-rel-kWh"].mean().sort_values(ascending=True)
+    st.bar_chart(data)
+
+    st.write(
+        """
+    #### Tensile vs Step 7 Temperature
+    """
+    )
+
+    data = df.groupby(["Tensile"])["Step7-Temperature-C-F"].mean().sort_values(ascending=True)
+    st.bar_chart(data)
+
+    st.write(
+        """
+    #### Tensile vs Step 7 Torque
+    """
+    )
+
+    data = df.groupby(["Tensile"])["Step7-Torque-Nm-ft/lbs"].mean().sort_values(ascending=True)
+    st.bar_chart(data)
